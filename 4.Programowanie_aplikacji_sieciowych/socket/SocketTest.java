b5d7b28583f39576199f58a0c86907c6cd8d8be3
Kamil Korbik <korbik.gm@gmail.com>
2021-03-25 16:16:43 +0100

    Added SocketTest listing

---
+++ 4.Programowanie_aplikacji_sieciowych/socket/SocketTest.java
@@ -0,0 +1,23 @@
+package socket;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+/**
+ * Program łączący się z zegarem atomowym w Boulder w stanie Kolorado  i wyświetlający uzyskany czas.
+ * @version 1.22 2018-03-17
+ * @author Cay Horstmann
+ */
+public class SocketTest {
+    public static void main(String[] args) throws IOException {
+        try (var s = new Socket("time-a.nist.gov", 13);
+             var in = new Scanner(s.getInputStream(), StandardCharsets.UTF_8)) {
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+                System.out.println(line);
+            }
+        }
+    }
+}
