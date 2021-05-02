628741eccd04b0845ef851a41de245db53bf6e15
Kamil Korbik <korbik.gm@gmail.com>
2021-05-02 23:55:46 +0200

    Added DateTimeFormater listing

--- 7.Internacjonalizacja/dateFormat/DateTimeFormaterTest.java
+++ 7.Internacjonalizacja/dateFormat/DateTimeFormaterTest.java
@@ -1,12 +1,22 @@
 package dateFormat;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Program demonstrujący formatowanie dat dla różnych lokalizatorów.
  * @version 1.01 2018-05-01
  * @author Cay Horstmann
  */
 public class DateTimeFormaterTest {
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
+        EventQueue.invokeLater(() ->
+        {
+            var frame = new DateTimeFormatterFrame();
+            frame.setTitle("DateFormatTest");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
     }
 }
